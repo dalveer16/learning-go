77dbbce510e1113e0a2bb4db463b049472b9f204
Dalveer Singh <dalveersinghrehsi@gmail.com>
2025-09-15 14:42:18 +0530

    ch1 - l2

--- CH1/l2.go
+++ CH1/l2.go
@@ -3,5 +3,10 @@ package CH1
 import "fmt"
 
 func L2() {
-	fmt.Println("hi from l2")
+	smsSendingLimit := 0
+	costPerSMS := 0.0
+	hasPermission := false
+	username := ""
+
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
